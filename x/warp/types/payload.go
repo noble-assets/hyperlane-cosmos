c02da27e6e3979a9fa4428548407d2dd2099abdb
stepit <48993133+0xstepit@users.noreply.github.com>
2025-11-21 12:39:58 +0100

    chore: support receiving payload in warp (#2)

--- x/warp/types/payload.go
+++ x/warp/types/payload.go
@@ -9,16 +9,33 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// Slice position and length of the Warp payload components.
+const (
+	RecipientIndex = 0
+	RecipientLen   = 32
+	AmountIndex    = RecipientIndex + RecipientLen
+	AmountLen      = 32
+	PayloadIndex   = AmountIndex + AmountLen
+)
+
+const (
+	// AddressLen is the length of an address in bytes.
+	AddressLen = 20
+	// PaddedLen represents the padded len of a 32 bytes array when used
+	// to represent a cross-chain address.
+	PaddedLen = RecipientLen - AddressLen
+)
+
 type WarpPayload struct {
 	recipient []byte
 	amount    big.Int
 }
 
 func NewWarpPayload(recipient []byte, amount big.Int) (WarpPayload, error) {
-	if len(amount.Bytes()) > 32 {
+	if len(amount.Bytes()) > AmountLen {
 		return WarpPayload{}, errors.New("amount is too long")
 	}
-	if len(recipient) > 32 {
+	if len(recipient) > RecipientLen {
 		return WarpPayload{}, errors.New("recipient address is too long")
 	}
 
@@ -26,26 +43,26 @@ func NewWarpPayload(recipient []byte, amount big.Int) (WarpPayload, error) {
 }
 
 func ParseWarpPayload(payload []byte) (WarpPayload, error) {
-	if len(payload) != 64 {
+	if len(payload) < PayloadIndex {
 		return WarpPayload{}, errors.New("payload is invalid")
 	}
 
-	amount := big.NewInt(0).SetBytes(payload[32:])
+	amount := big.NewInt(0).SetBytes(payload[AmountIndex : AmountIndex+AmountLen])
 
 	return WarpPayload{
-		recipient: payload[0:32],
+		recipient: payload[RecipientIndex : RecipientIndex+RecipientLen],
 		amount:    *amount,
 	}, nil
 }
 
 func isZeroPadded(bz []byte) bool {
-	return bytes.HasPrefix(bz, make([]byte, 12))
+	return bytes.HasPrefix(bz, make([]byte, PaddedLen))
 }
 
 func (p WarpPayload) GetCosmosAccount() sdk.AccAddress {
 	// If address is zero padded it is a 20-byte default cosmos address
 	if isZeroPadded(p.recipient) {
-		return p.recipient[12:32]
+		return p.recipient[PaddedLen:RecipientLen]
 	}
 	// if the address is not zero-padded, it might be a 32-byte address
 	return p.recipient
@@ -63,12 +80,12 @@ func (p WarpPayload) Amount() *big.Int {
 
 func (p WarpPayload) Bytes() []byte {
 	intBytes := p.amount.Bytes()
-	amountBytes := make([]byte, 32)
-	copy(amountBytes[32-len(intBytes):], intBytes)
+	amountBytes := make([]byte, AmountLen)
+	copy(amountBytes[AmountLen-len(intBytes):], intBytes)
 
 	recBytes := p.recipient
-	receiverBytes := make([]byte, 32)
-	copy(receiverBytes[32-len(recBytes):], recBytes)
+	receiverBytes := make([]byte, RecipientLen)
+	copy(receiverBytes[RecipientLen-len(recBytes):], recBytes)
 
 	return slices.Concat(
 		receiverBytes,
